6250a63a7d970d15f4547448f05d6e7692431703
saikrishna-dev <saikrishnacr2012@gmail.com>
2022-11-21 18:45:15 +0000

    class to function

--- src/Signup.js
+++ src/Signup.js
@@ -1,172 +1,142 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Constants from "./Constants";
 import Encryption from "./Encryption";
 
 import { Navigate, Link } from "react-router-dom";
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userId: "",
-      passwordHash: "",
-      email: "",
-      dataLoaded: false,
-      data: "",
-      statusCode: "",
-      passwordMatch: false,
-      confirmPasswordDirty: false,
-    };
+function Signup(props) {
+  const [userId, setUserId] = useState("");
+  const [passwordHash, setPasswordHash] = useState("");
+  const [email, setEmail] = useState("");
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [statusCode, setStatusCode] = useState("");
+  const [data, setData] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(false);
+  const [confirmPasswordDirty, setConfirmPasswordDirty] = useState(false);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.matchPassword = this.matchPassword.bind(this);
-    this.signup = this.signup.bind(this);
-    this.flagDirty = this.flagDirty.bind(this);
+  function handleChange(event) {
+    if (event.target.type === "text") {
+      setUserId(event.target.value);
+    }
+    if (event.target.type === "password") {
+      setPasswordHash(event.target.value);
+    }
+    if (event.target.type === "email") {
+      setEmail(event.target.value);
+    }
   }
-
-  handleChange(event) {
-    this.setState({
-      userId:
-        event.target.type === "text" ? event.target.value : this.state.userId,
-      passwordHash:
-        event.target.type === "password"
-          ? event.target.value
-          : this.state.passwordHash,
-      email:
-        event.target.type === "email" ? event.target.value : this.state.email,
-    });
-  }
-
-  handleSubmit(event) {
-    this.signup();
+  function handleSubmit(event) {
+    signup();
     event.preventDefault();
   }
 
-  matchPassword(event) {
-    if (event.target.value === this.state.passwordHash) {
-      this.setState({
-        passwordMatch: true,
-      });
+  function matchPassword(event) {
+    if (event.target.value === passwordHash) {
+      setPasswordMatch(true);
     } else {
-      this.setState({
-        passwordMatch: false,
-      });
+      setPasswordMatch(false);
     }
   }
 
-  async signup() {
+  async function signup() {
     axios
       .post(Constants.BASE_API_URL + "/signup", {
-        userId: this.state.userId,
-        passwordHash: await Encryption.encryptPassword(this.state.passwordHash),
-        email: this.state.email,
+        userId: userId,
+        passwordHash: await Encryption(passwordHash),
+        email: email,
       })
       .then(
         (response) => {
-          this.setState({
-            dataLoaded: true,
-            data: response.data,
-            statusCode: 204,
-          });
+          setDataLoaded(true);
+          setData(response.data);
+          setStatusCode(204);
         },
         (err) => {
           if (err.response == null) {
-            this.setState({
-              dataLoaded: true,
-              data: "Failed to Connect",
-              statusCode: 500,
-            });
+            setDataLoaded(true);
+            setData("Failed to connect");
+            setStatusCode(500);
           } else {
-            this.setState({
-              dataLoaded: true,
-              data:
-                "{ status: " +
+            setDataLoaded(true);
+            setData(
+              "{ status: " +
                 err.response.status +
                 ",\n message: " +
                 err.response.data +
-                " }",
-              statusCode: err.response.status,
-            });
+                " }"
+            );
+            setStatusCode(err.response.status);
           }
         }
       )
       .catch((err) => {
-        this.setState({
-          dataLoaded: true,
-          data: "Failed to connect",
-          statusCode: 500,
-        });
+        setDataLoaded(true);
+        setData("Failed to connect");
+        setStatusCode(500);
       });
   }
 
-  flagDirty(event) {
+  function flagDirty(event) {
     if (event.target.value === "") {
-      this.setState({
-        confirmPasswordDirty: false,
-      });
+      setConfirmPasswordDirty(false);
     } else {
-      this.setState({
-        confirmPasswordDirty: true,
-      });
+      setConfirmPasswordDirty(true);
     }
   }
 
-  render() {
-    if (this.state.dataLoaded) {
-      this.props.handleDataLoad(this.state.data, this.state.statusCode);
-      return <Navigate push to="/home" />;
-    }
-    return (
-      <div className="Auth-form-container">
-        <form className="Auth-form" onSubmit={this.handleSubmit}>
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign Up</h3>
-            <div className="text-center">
-              Already registered?{" "}
-              <Link to="/signin">
-                <span id="loginRedirect" className="redirect">
-                  Signin
-                </span>
-              </Link>
-            </div>
-            <div className="form-group mt-3">
-              <label>Full Name</label>
-              <input
-                type="text"
-                className="form-control mt-1"
-                placeholder="e.g Jane Doe"
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Email Address"
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
+  if (dataLoaded) {
+    props.handleDataLoad(data, statusCode);
+    return <Navigate push to="/home" />;
   }
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form" onSubmit={handleSubmit}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <Link to="/signin">
+              <span id="loginRedirect" className="redirect">
+                Signin
+              </span>
+            </Link>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
